4294632d92155e673ab5a47630ee77fa995e2300
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-10-10 16:56:52 +0000

    git version info

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+var git = require('git-node');
+
 var path = require('path');
 
 module.exports = function (app, endpoint) {
+    var resolve = function (fileName) {
+        return path.resolve(process.cwd(), fileName);
+    };
+
     app.get(endpoint || '/info', function (req, res) {
-        res.send(require(path.resolve(process.cwd(), './package')));
+        res.send(require(resolve('./package')));
+    });
+
+    app.get('/version', function (req, res) {
+        var repo = git.repo(resolve('.git'));
+
+        repo.load('HEAD', function (err, commit) {
+            if (err) {
+                return res.send(500, 'could not lookup repo for server');
+            }
+
+            res.send(commit.body);
+        });
     });
 };
