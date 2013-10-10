378623e0d1ea59e7acfda113d614ba49522828f4
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-10-10 14:50:48 +0000

    pull package from process.cwd()

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var path = require('path');
+
 module.exports = function (app, endpoint) {
     app.get(endpoint || '/info', function (req, res) {
-        res.send(require('./package'));
+        res.send(require(path.resolve(process.cwd(), './package')));
     });
 };
